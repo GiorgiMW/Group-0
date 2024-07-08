10b051c1fd2e2da65020cd128ad61dab8374860c
GiorgiMW <162885204+GiorgiMW@users.noreply.github.com>
2024-07-08 14:47:04 +0400

    commit

---
+++ day 51/classwork/script.js
@@ -0,0 +1,15 @@
+const me = {
+    FirstName : "Giorgi",
+    LastName : "Motsonelidze",
+    Age : 16,
+    Country : "Georgia",
+}
+
+const teacher = {
+    FirstName : "Luka",
+    LastName : "Tskhvaradze",
+    Age : 17,
+    job : "Mentor",
+}
+
+console.log(me.FirstName + me.LastName + " " + "and my teacher is " + teacher.FirstName + teacher.LastName)
